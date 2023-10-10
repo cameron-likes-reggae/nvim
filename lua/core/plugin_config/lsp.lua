4dd448012757d103582129bd36fdfddb8ff8ab4a
Cameron Airlie <Cameron@Work>
2023-10-10 16:21:35 +0100

    - fixed neo tree migration change

--- lua/core/plugin_config/lsp.lua
+++ lua/core/plugin_config/lsp.lua
@@ -1,11 +1,20 @@
 local lsp_zero = require('lsp-zero')
 
 lsp_zero.on_attach(function(client, bufnr)
-  -- see :help lsp-zero-keybindings
-  -- to learn the available actions
-  lsp_zero.default_keymaps({buffer = bufnr})
+  lsp_zero.default_keymaps({ buffer = bufnr })
   lsp_zero.async_autoformat(client, bufnr)
 end)
+
+-- add 'vim' to lua globals
+require('lspconfig').lua_ls.setup {
+  settings = {
+    Lua = {
+      diagnostics = {
+        globals = { 'vim' }
+      }
+    }
+  }
+}
 
 require('mason').setup({})
 require('mason-lspconfig').setup({
