9344590063073329a12a4ef950312542bf03b44f
Cameron <cam@camerons-pc>
2023-10-10 20:16:24 +0100

    removed unnecessary settings

--- lua/core/plugin_config/lsp.lua
+++ lua/core/plugin_config/lsp.lua
@@ -7,33 +7,6 @@ lsp_zero.on_attach(function(client, bufnr)
   lsp_zero.async_autoformat(client, bufnr)
 end)
 
-require('lspconfig').lua_ls.setup {
-  settings = {
-    Lua = {
-      runtime = {
-        -- Tell the language server which version of Lua you're using
-        -- (most likely LuaJIT in the case of Neovim)
-        version = 'LuaJIT',
-      },
-      diagnostics = {
-        -- Get the language server to recognize the `vim` global
-        globals = {
-          'vim',
-          'require'
-        },
-      },
-      workspace = {
-        -- Make the server aware of Neovim runtime files
-        library = vim.api.nvim_get_runtime_file("", true),
-      },
-      -- Do not send telemetry data containing a randomized but unique identifier
-      telemetry = {
-        enable = false,
-      },
-    },
-  },
-}
-
 -- add 'vim' to lua globals
 require('lspconfig').lua_ls.setup {
   settings = {
