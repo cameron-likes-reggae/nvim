7492f3a5fbe6b9101c1a768cbd705fe80704ad30
Cameron <cam@camerons-pc>
2023-10-11 21:38:05 +0100

    Added current buffer fuzzy find with <leader>/

--- lua/core/plugin_config/telescope.lua
+++ lua/core/plugin_config/telescope.lua
@@ -1,5 +1,11 @@
 local builtin = require('telescope.builtin')
 
+vim.keymap.set('n', '<leader>/', function()
+  require("telescope.builtin").current_buffer_fuzzy_find({
+    previewer = false
+  })
+end)
+
 vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
 vim.keymap.set('n', '<Space><Space>', builtin.oldfiles, {})
 vim.keymap.set('n', '<Space>fg', builtin.live_grep, {})
