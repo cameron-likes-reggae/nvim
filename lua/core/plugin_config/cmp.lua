27bee04ed14bcb4fc2126d21ae74c559dd8bc175
Cameron <cameron@workpc>
2024-04-19 08:44:41 +0100

    fixes

--- lua/core/plugin_config/cmp.lua
+++ lua/core/plugin_config/cmp.lua
@@ -1,86 +1,99 @@
-local cmp = require('cmp')
-local cmp_action = require('lsp-zero').cmp_action()
+local cmp = require("cmp")
+local cmp_action = require("lsp-zero").cmp_action()
 
-local lspkind = require('lspkind')
+require("cmp_nvim_ultisnips").setup({
+	filetype_source = "ultisnips_default",
+	show_snippets = "all",
+	documentation = function(snippet)
+		return snippet.description .. "\n\n" .. snippet.value
+	end,
+})
+
+local lspkind = require("lspkind")
 
 cmp.setup({
-  snippet = {
-    expand = function(args)
-      -- require('ultisnips').lsp_expand(args.body)
-      vim.fn["UltiSnips#Anon"](args.body)
-    end,
-  },
-  view = {
-    entries = {
-      name            = 'custom',
-      selection_order = 'near_cursor'
-    }
-  },
-  window = {
-    completion = {
-      border       = 'none',
-      winhighlight = 'NormalFloat:NormalFloat,FloatBorder:NormalFloat',
-      -- winhighlight = "Normal:Pmenu,FloatBorder:Pmenu,Search:None",
-      max_height   = 30,
-      col_offset   = -3,
-      side_padding = 0,
-      max_width    = 80,
-      zindex       = 50,
-    },
-    documentation = {
-      border = 'none',
-      winhighlight = 'NormalFloat:NormalFloat,FloatBorder:NormalFloat',
-      max_height = 15,
-      max_width = 80,
-      zindex = 50,
-    },
-  },
-  preselect = 'item',
-  completion = {
-    completeopt = 'menu,menuone,noinsert'
-  },
-  sources = {
-    { name = 'nvim_lsp',  keyword_length = 2, max_item_count = 10, priority = 10, group_index = 1 },
-    { name = 'ultisnips', keyword_length = 2, max_item_count = 10, priority = 10, group_index = 1 },
-  },
-  performance = {
-    trigger_debouce_time = 500,
-    throttle = 550,
-    fetching_timeout = 80,
-  },
-  formatting = {
-    fields = { "kind", "abbr", "menu" },
-    format = function(entry, vim_item)
-      local kind = lspkind.cmp_format({
-        mode = 'symbol_text',
-        with_text = true,
-        maxwidth = 50,
-        menu = ({
-          nvim_lsp = "[LSP]",
-          ultisnips = "[UltiSnips]",
-        }),
-      })(entry, vim_item)
-      local strings = vim.split(kind.kind, "%s", { trimempty = true })
-      kind.kind = " " .. (strings[1] or "") .. " "
-      kind.menu = "    (" .. (strings[2] or "") .. ")"
+	snippet = {
+		expand = function(args)
+			vim.fn["UltiSnips#Anon"](args.body)
+		end,
+	},
+	view = {
+		entries = {
+			name = "custom",
+			selection_order = "near_cursor",
+		},
+	},
+	window = {
+		completion = {
+			border = "single",
+			winhighlight = "NormalFloat:NormalFloat,FloatBorder:NormalFloat",
+			-- winhighlight = "Normal:Pmenu,FloatBorder:Pmenu,Search:None",
+			max_height = 60,
+			col_offset = -3,
+			side_padding = 0,
+			max_width = 80,
+			zindex = 50,
+		},
+		documentation = {
+			border = "single",
+			winhighlight = "NormalFloat:NormalFloat,FloatBorder:NormalFloat",
+			max_height = 60,
+			max_width = 80,
+			zindex = 50,
+		},
+	},
+	preselect = "item",
+	completion = {
+		completeopt = "menu,menuone,noinsert",
+	},
+	sources = {
+		-- { name = "nvim_lsp", keyword_length = 2, max_item_count = 10, priority = 5, group_index = 1 },
+		-- { name = "ultisnips", keyword_length = 2, max_item_count = 10, priority = 10, group_index = 1 },
+		{ name = "luasnip" },
+		{ name = "nvim_lsp", max_item_count = 20 },
+		{ name = "calc" },
+		{ name = "path" },
+		-- { name = "buffer" },
+	},
+	-- performance = {
+	-- 	trigger_debouce_time = 500,
+	-- 	throttle = 550,
+	-- 	fetching_timeout = 80,
+	-- },
+	formatting = {
+		fields = { "kind", "abbr", "menu" },
+		format = function(entry, vim_item)
+			local kind = lspkind.cmp_format({
+				mode = "symbol_text",
+				with_text = true,
+				maxwidth = 50,
+				menu = {
+					nvim_lsp = "[LSP]",
+					ultisnips = "[UltiSnips]",
+					calc = "[Calc]",
+					path = "[Path]",
+				},
+			})(entry, vim_item)
+			local strings = vim.split(kind.kind, "%s", { trimempty = true })
+			kind.kind = " " .. (strings[1] or "") .. " "
+			--kind.menu = "    (" .. (strings[2] or "") .. ")"
+			return kind
+		end,
+	},
+	mapping = cmp.mapping.preset.insert({
+		-- Ctrl+Space to trigger completion menu
+		["<C-l>"] = cmp.mapping.complete(),
 
-      return kind
-    end
-  },
-  mapping = cmp.mapping.preset.insert({
-    -- Ctrl+Space to trigger completion menu
-    ['<C-l>'] = cmp.mapping.complete(),
+		-- Navigate between snippet placeholder
+		["<C-f>"] = cmp_action.luasnip_jump_forward(),
+		["<C-b>"] = cmp_action.luasnip_jump_backward(),
 
-    -- Navigate between snippet placeholder
-    ['<C-f>'] = cmp_action.luasnip_jump_forward(),
-    ['<C-b>'] = cmp_action.luasnip_jump_backward(),
-
-    -- Scroll up and down in the completion documentation
-    ['<C-u>'] = cmp.mapping.scroll_docs(-4),
-    ['<C-d>'] = cmp.mapping.scroll_docs(4),
-  }),
-  experimental = {
-    -- native_menu = true,
-    -- ghost_text = true
-  }
+		-- Scroll up and down in the completion documentation
+		["<C-u>"] = cmp.mapping.scroll_docs(-4),
+		["<C-d>"] = cmp.mapping.scroll_docs(4),
+	}),
+	experimental = {
+		-- native_menu = true,
+		-- ghost_text = true
+	},
 })
--- lua/core/plugin_config/lsp.lua
+++ lua/core/plugin_config/lsp.lua
@@ -1,17 +1,17 @@
-local lsp_zero = require('lsp-zero')
+local lsp_zero = require("lsp-zero")
 
 -- Format on save
-local augroup = vim.api.nvim_create_augroup('LspFormatting', {})
+local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
 local lsp_format_on_save = function(client, bufnr)
-  vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
-  vim.api.nvim_create_autocmd('BufWritePost', {
-    group = augroup,
-    buffer = bufnr,
-    callback = function(args)
-      -- vim.lsp.buf.format()
-      require("conform").format({ bufnr = args.buf })
-    end,
-  })
+	vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+	vim.api.nvim_create_autocmd("BufWritePost", {
+		group = augroup,
+		buffer = bufnr,
+		callback = function(args)
+			-- vim.lsp.buf.format()
+			require("conform").format({ bufnr = args.buf })
+		end,
+	})
 end
 
 -- vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover,
@@ -23,49 +23,52 @@ end
 -- )
 
 lsp_zero.on_attach(function(client, bufnr)
-  local opts = { buffer = bufnr }
+	local opts = { buffer = bufnr }
 
-  -- lsp_zero.async_autoformat(client, bufnr)
-  -- lsp_format_on_save(client, bufnr)
+	-- lsp_zero.async_autoformat(client, bufnr)
+	lsp_format_on_save(client, bufnr)
 
-  vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
-  -- vim.keymap.set('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<cr>', opts)
-  vim.keymap.set('n', 'gd', "<cmd>lua require'telescope.builtin'.lsp_definitions{}<cr>", opts)
-  vim.keymap.set('n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<cr>', opts)
-  -- vim.keymap.set('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<cr>', opts)
-  vim.keymap.set('n', 'gi', "<cmd>lua require'telescope.builtin'.lsp_implementations{}<cr>", opts)
-  -- vim.keymap.set('n', 'go', '<cmd>lua vim.lsp.buf.type_definition()<cr>', opts)
-  vim.keymap.set('n', 'go', "<cmd>lua require'telescope.builtin'.lsp_type_definitions{}<cr>", opts)
-  vim.keymap.set('n', 'gr', '<cmd>lua vim.lsp.buf.references()<cr>', opts)
-  vim.keymap.set('n', 'gs', '<cmd>lua vim.lsp.buf.signature_help()<cr>', opts)
-  vim.keymap.set('n', '<F2>', '<cmd>lua vim.lsp.buf.rename()<cr>', opts)
-  vim.keymap.set({ 'n', 'x' }, '<F3>', '<cmd>lua vim.lsp.buf.format({async = true})<cr>', opts)
-  vim.keymap.set('n', '<F4>', '<cmd>lua vim.lsp.buf.code_action()<cr>', opts)
+	vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
+	-- vim.keymap.set('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<cr>', opts)
+	vim.keymap.set("n", "gd", "<cmd>lua require'telescope.builtin'.lsp_definitions{}<cr>", opts)
+	vim.keymap.set("n", "gD", "<cmd>lua vim.lsp.buf.declaration()<cr>", opts)
+	-- vim.keymap.set('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<cr>', opts)
+	vim.keymap.set("n", "gi", "<cmd>lua require'telescope.builtin'.lsp_implementations{}<cr>", opts)
+	-- vim.keymap.set('n', 'go', '<cmd>lua vim.lsp.buf.type_definition()<cr>', opts)
+	vim.keymap.set("n", "go", "<cmd>lua require'telescope.builtin'.lsp_type_definitions{}<cr>", opts)
+	vim.keymap.set("n", "gr", "<cmd>lua vim.lsp.buf.references()<cr>", opts)
+	vim.keymap.set("n", "gs", "<cmd>lua vim.lsp.buf.signature_help()<cr>", opts)
+	vim.keymap.set("n", "<F2>", "<cmd>lua vim.lsp.buf.rename()<cr>", opts)
+	vim.keymap.set({ "n", "x" }, "<F3>", "<cmd>lua vim.lsp.buf.format({async = true})<cr>", opts)
+	vim.keymap.set("n", "<F4>", "<cmd>lua vim.lsp.buf.code_action()<cr>", opts)
 
-  vim.keymap.set('n', 'gl', '<cmd>lua vim.diagnostic.open_float()<cr>', opts)
-  vim.keymap.set('n', '[d', '<cmd>lua vim.diagnostic.goto_prev()<cr>', opts)
-  vim.keymap.set('n', ']d', '<cmd>lua vim.diagnostic.goto_next()<cr>', opts)
+	vim.keymap.set("n", "gl", "<cmd>lua vim.diagnostic.open_float()<cr>", opts)
+	vim.keymap.set("n", "[d", "<cmd>lua vim.diagnostic.goto_prev()<cr>", opts)
+	vim.keymap.set("n", "]d", "<cmd>lua vim.diagnostic.goto_next()<cr>", opts)
 end)
 
-local capabilities = require('cmp_nvim_lsp').default_capabilities()
+local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
-require('mason').setup({})
-require('mason-lspconfig').setup({
-  ensure_installed = { "cssls", "jsonls", "lua_ls", "tsserver" },
-  handlers = {
-    function(server)
-      require("lspconfig")[server].setup {
-        handlers = {
-          ["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, { border = "none" }),
-          ["textDocument/signatureHelp"] = vim.lsp.with(vim.lsp.handlers.signatureHelp, { border = "none" }),
-        },
-        capabilities = capabilities,
-        on_attach = function(client, bufnr)
-          lsp_zero.on_attach(client, bufnr)
-        end,
-      }
-    end,
-  },
+require("mason").setup({})
+require("mason-lspconfig").setup({
+	ensure_installed = { "cssls", "jsonls", "lua_ls", "tsserver" },
+	handlers = {
+		function(server)
+			require("lspconfig")[server].setup({
+				handlers = {
+					["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, { border = "single" }),
+					["textDocument/signatureHelp"] = vim.lsp.with(
+						vim.lsp.handlers.signature_help,
+						{ border = "single" }
+					),
+				},
+				on_attach = function(client, bufnr)
+					lsp_zero.on_attach(client, bufnr)
+				end,
+				capabilities = capabilities,
+			})
+		end,
+	},
 })
 
 -- require("lspconfig").tsserver.setup({
