123bf4168d3bbfe0fc5fa838bd60c55ac756d45b
Cameron <cameron@work>
2024-05-02 16:58:38 +0100

    added colored border

--- lua/core/plugin_config/overseer.lua
+++ lua/core/plugin_config/overseer.lua
@@ -1,4 +1,14 @@
-require("overseer").setup()
+require("overseer").setup({
+	task_list = {
+		default_detail = 2,
+		direction = "right",
+	},
+	component_aliases = {
+		default = {
+			{ "on_output_summarize", max_lines = 10 },
+		},
+	},
+})
 
 vim.keymap.set("n", "<leader>oo", "<cmd>OverseerToggle!<cr>", { noremap = true, silent = true })
 vim.keymap.set("n", "<leader>oa", "<cmd>OverseerQuickAction<cr>", { noremap = true, silent = true })
